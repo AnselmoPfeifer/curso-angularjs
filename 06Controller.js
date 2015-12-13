296a637e7578d076a6bf28559e7a125fe44704c1
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2015-12-13 18:28:15 -0300

    Finalizando aula 07 - Loops com ngRepeat

---
+++ 06Controller.js
@@ -0,0 +1,5 @@
+app.controller("07Controller", function($scope){
+	$scope.nomes = ['Anselmo', 'Jooa', 'Pedro','Maria'];
+
+	console.log($scope.nomes);
+});
---
+++ js/06Controller.js
@@ -0,0 +1,35 @@
+app.controller("07Controller", function($scope){
+	$scope.nomes = ['Anselmo', 'Jooa', 'Pedro','Maria'];
+
+	$scope.pessoas = [];
+	$scope.pessoas.push(
+		{ nome: "Anselmo Pfeifer", idade : 35, status : false }
+	);
+
+	$scope.pessoas.push(
+		{ nome: "Pfeifer Anselmo", idade : 35, status : false }
+	);
+
+	$scope.pessoas.push(
+		{ nome: "Pfeifer", idade : 35, status : false }
+	);
+
+	$scope.pessoas.push(
+		{ nome: "Anselmo", idade : 35, status : false }
+	);
+
+	console.log($scope.nomes);
+	console.log($scope.pessoas);
+
+	$scope.adicionarPessoa = function(){
+		var nome = document.getElementById("nomepesssoa");
+		var idade = document.getElementById('idadepessoa');
+
+		$scope.pessoas.push(
+			{nome : nome.value, idade : idade.value }
+		);
+		nome.value = "";
+		idade.value = "";
+	}
+	
+});
